b7a4c531ceb586af3f9097b68e1e798ff5fe6770
liuwohe <2360826015@qq.com>
2022-04-19 17:43:40 +0800

    删除公告

--- src/main/java/com/liuwohe/communitymis/entity/CarInfo.java
+++ src/main/java/com/liuwohe/communitymis/entity/CarInfo.java
@@ -17,7 +17,7 @@ public class CarInfo implements Serializable {
     /**
      * ID
      */
-    @TableId
+    @TableId(type = IdType.UUID)
     private String carInfoId;
 
     /**
@@ -52,57 +52,4 @@ public class CarInfo implements Serializable {
 
     @TableField(exist = false)
     private static final long serialVersionUID = 1L;
-
-    @Override
-    public boolean equals(Object that) {
-        if (this == that) {
-            return true;
-        }
-        if (that == null) {
-            return false;
-        }
-        if (getClass() != that.getClass()) {
-            return false;
-        }
-        CarInfo other = (CarInfo) that;
-        return (this.getCarInfoId() == null ? other.getCarInfoId() == null : this.getCarInfoId().equals(other.getCarInfoId()))
-            && (this.getUserId() == null ? other.getUserId() == null : this.getUserId().equals(other.getUserId()))
-            && (this.getCarowner() == null ? other.getCarowner() == null : this.getCarowner().equals(other.getCarowner()))
-            && (this.getNumPlate() == null ? other.getNumPlate() == null : this.getNumPlate().equals(other.getNumPlate()))
-            && (this.getType() == null ? other.getType() == null : this.getType().equals(other.getType()))
-            && (this.getColor() == null ? other.getColor() == null : this.getColor().equals(other.getColor()))
-            && (this.getPhone() == null ? other.getPhone() == null : this.getPhone().equals(other.getPhone()));
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((getCarInfoId() == null) ? 0 : getCarInfoId().hashCode());
-        result = prime * result + ((getUserId() == null) ? 0 : getUserId().hashCode());
-        result = prime * result + ((getCarowner() == null) ? 0 : getCarowner().hashCode());
-        result = prime * result + ((getNumPlate() == null) ? 0 : getNumPlate().hashCode());
-        result = prime * result + ((getType() == null) ? 0 : getType().hashCode());
-        result = prime * result + ((getColor() == null) ? 0 : getColor().hashCode());
-        result = prime * result + ((getPhone() == null) ? 0 : getPhone().hashCode());
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", carInfoId=").append(carInfoId);
-        sb.append(", userId=").append(userId);
-        sb.append(", carowner=").append(carowner);
-        sb.append(", numPlate=").append(numPlate);
-        sb.append(", type=").append(type);
-        sb.append(", color=").append(color);
-        sb.append(", phone=").append(phone);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
-    }
 }
